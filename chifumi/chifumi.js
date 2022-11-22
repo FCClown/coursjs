5fd3909754378ff4e865b4341620ff2b858bc5c9
FC Clown <78288064+FCClown@users.noreply.github.com>
2022-11-22 15:35:11 +0100

    Reupload chifumi

--- chifumi/chifumi.js
+++ chifumi/chifumi.js
@@ -1,64 +1,76 @@
-function comparer(choix1, choix2) {
-    if (choix1 === choix2) {
-      return 'Egalité !';
-    } else if (choix1 === 'pierre') {
-      if (choix2 === 'ciseaux') {
-        return 'pierre';
-      } else if (choix2 === 'feuille') {
-        return 'feuille';
-      }
-    } else if (choix1 === 'feuille') {
-      if (choix2 === 'pierre') {
-        return 'feuille';
-      } else if (choix2 === 'ciseaux') {
-        return 'ciseaux';
-      }
-    } else if (choix1 === 'ciseaux') {
-      if (choix2 === 'pierre') {
-        return 'pierre';
-      } else if (choix2 === 'feuille') {
-        return 'ciseaux';
-      }
+let scoreUtilisateur = 0
+let scoreOrdinateur = 0
+
+for(i = 1; i <= 3; i++) {
+
+    let resultRandom
+
+    // Utilisateur
+    const choixUtilisateur = prompt("Choisir votre signe !").toLowerCase()
+    //console.log(choixUtilisateur)
+
+    // bot
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
     }
-  }
-  // début de partie
-  var scoreOrdi = 0;
-  var scoreJoueur = 0;
-  for (var manche = 0; manche < 3; manche++) {
-    // choix aléatoire de l'ordinateur
-    var choixOrdi = Math.random();
-    if (choixOrdi < 0.34) {
-      choixOrdi = 'pierre';
-    } else if(choixOrdi <= 0.67) {
-      choixOrdi = 'feuille';
-    } else {
-      choixOrdi = 'ciseaux';
+
+    let random = getRandomInt(3)
+    //console.log(random)
+
+    if(random === 0) {
+        console.log("BOT : papier")
+        resultRandom = "papier"
+    } else if (random === 1) {
+        console.log("BOT : pierre")
+        resultRandom = "pierre"
+    } else if(random === 2) {
+        console.log("BOT : ciseaux")
+        resultRandom = "ciseaux"
     }
-    console.log('Ordinateur:', choixOrdi);
-    // choix du joueur
-    var choixUtilisateur = prompt('Choisissez-vous pierre, feuille, ou ciseaux ?').toLowerCase();;
-    // déterminer qui remporte la manche
-    var resultat = comparer(choixUtilisateur, choixOrdi);
-    // afficher les résultats de la manche, et mettre à jour les scores
-    if (resultat === 'Egalité !') {
-      alert('Egalité !');
-      scoreJoueur++;
-      scoreOrdi++;
-    } else if (resultat === choixUtilisateur) {
-      alert('Gagnant de la manche : ' + resultat + ' (joueur)');
-      scoreJoueur++;
-    } else {
-      alert('Gagnant de la manche : ' + resultat + ' (ordi)');
-      scoreOrdi++;
+    //console.log(resultRandom)
+
+    // Resultat
+    switch(resultRandom) {
+        case "papier":
+            if(choixUtilisateur === "pierre") {
+                scoreOrdinateur+=1
+                //console.log(scoreOrdinateur)
+                alert("L'ordinateur gagne")
+            } else if(choixUtilisateur === "ciseaux") {
+                scoreUtilisateur+=1
+                //console.log(scoreUtilisateur)
+                alert("L'utilisateur gagne")
+            } else if(choixUtilisateur === "papier") {
+                alert("Egalité")
+            }
+            break
+        case "ciseaux":
+            if(choixUtilisateur === "pierre") {
+                scoreUtilisateur+=1
+                //console.log(scoreUtilisateur)
+                alert("L'utilisateur gagne")
+            } else if(choixUtilisateur === "ciseaux") {
+                alert("Egalité")
+            } else if(choixUtilisateur === "papier") {
+                scoreOrdinateur+=1
+                //console.log(scoreOrdinateur)
+                alert("L'ordinateur gagne")
+            }
+            break
+        case "pierre":
+            if(choixUtilisateur === "pierre") {
+                alert("Egalité")
+            } else if(choixUtilisateur === "ciseaux") {
+                scoreOrdinateur+=1
+                //console.log(scoreOrdinateur)
+                alert("L'ordinateur gagne")
+            } else if(choixUtilisateur === "papier") {
+                scoreUtilisateur+=1
+                //console.log(scoreUtilisateur)
+                alert("L'utilisateur gagne")
+            }
+            break
     }
-  }
-  // afficher les résultats du jeu
-  var message;
-  if (scoreOrdi === scoreJoueur) {
-    message = 'Égalité !';
-  } else if (scoreOrdi > scoreJoueur) {
-    message = 'Perdu !';
-  } else {
-    message = 'Gagné !';
-  }
-  alert(message + ' Votre score: ' + scoreJoueur + ', ordi: ' + scoreOrdi);
+    console.log("Score Ordinateur " + scoreOrdinateur)
+    console.log("Score Utilisateur " + scoreUtilisateur)
+}
